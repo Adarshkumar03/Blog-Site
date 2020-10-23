a88a9b2462ee1465b551d54c477832f56c12525c
Adarsh Kumar <54439573+Adarshkumar03@users.noreply.github.com>
2020-10-24 00:35:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,15 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+const http = require('http');
 const port = process.env.PORT || 3000
-app.listen(port, function() {
-  console.log("Server started on port 3000");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
 });
